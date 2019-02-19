189bdef56f292d7e0409854d515d2c6817b67295
Nyi Nyi Htun Lwin <nnhl.mgk@gmail.com>
2019-02-19 16:43:17 +0630

    Business logics added.

--- Destini/Controller/ViewController.swift
+++ Destini/Controller/ViewController.swift
@@ -35,30 +35,85 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyTextView: UILabel!
     
     // TODO Step 5: Initialise instance variables here
-    
+    var currentStoryNo : Int = 1
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
+    
+      goToStory(storyNo: 1)
         
     }
 
     
     // User presses one of the buttons
     @IBAction func buttonPressed(_ sender: UIButton) {
-    
-        // TODO Step 4: Write an IF-Statement to update the views
-                
-        // TODO Step 6: Modify the IF-Statement to complete the story
-        
-    
+        if(sender.tag == 1){
+            if(currentStoryNo == 1){
+                goToStory(storyNo : 3)
+            }else if(currentStoryNo == 2){
+                goToStory(storyNo: 3)
+            }else if (currentStoryNo == 3){
+                goToStory(storyNo: 6)
+            }
+        }else{
+            if(currentStoryNo == 1){
+                goToStory(storyNo: 2)
+            }else if(currentStoryNo == 2){
+                goToStory(storyNo: 4)
+            }else if(currentStoryNo == 3){
+                goToStory(storyNo: 5)
+            }
+        }
     }
     
-
+    func goToStory(storyNo : Int) {
+        currentStoryNo = storyNo
+        showButtons()
+        if(storyNo == 1){
+            storyTextView.text = story1
+            topButton.setTitle(answer1a, for: .normal)
+            bottomButton.setTitle(answer1b, for: .normal)
+        }else if (storyNo == 2){
+            storyTextView.text = story2
+            topButton.setTitle(answer2a, for: .normal)
+            bottomButton.setTitle(answer2b, for: .normal)
+        }else if (storyNo == 3){
+            storyTextView.text = story3
+            topButton.setTitle(answer3a, for: .normal)
+            bottomButton.setTitle(answer3b, for: .normal)
+        }else if (storyNo == 4){
+            storyTextView.text = story4
+            endStroy()
+        }else if (storyNo == 5){
+            storyTextView.text = story5
+            endStroy()
+        }else if (storyNo == 6){
+            storyTextView.text = story6
+            endStroy()
+        }
+    }
+    
+    func endStroy() {
+        hideButtons()
+        let alert = UIAlertController(title: "The End", message: "Do you want to start againg?", preferredStyle: .actionSheet)
+        let actionYes = UIAlertAction(title: "Yes", style: .default) { (actionYes) in
+            self.goToStory(storyNo: 1)
+        }
+        alert.addAction(actionYes)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func hideButtons() {
+        topButton.isHidden = true
+        bottomButton.isHidden = true
+    }
+    
+    func showButtons() {
+        topButton.isHidden = false
+        bottomButton.isHidden = false
+    }
 
 
 }
